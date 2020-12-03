541ecabfbbe926480ceef127e62e3be17d050afd
phendev <stephen.bewers@gmail.com>
2020-12-03 18:48:22 +0000

    Solution for Day 3, Part 2

--- aoc2020_day3.js
+++ aoc2020_day3.js
@@ -7,19 +7,59 @@ fetch(inputFile)
     array = data.split("\n");
   })
 
-// Part 1 solution
-const countTrees = (arr) => {
+
+// Solution to Part 1. Pass in the number of steps to the right and down.
+const countTrees = (right, down) => {
     let charIndex = 0;
     let treeCount = 0;
 
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i].charAt(charIndex) === "#") {
+    for (i = 0; i < array.length; i += down) {
+        if (array[i].charAt(charIndex) === "#") {
             treeCount++;
         }
-        charIndex += 3;
+        charIndex += right;
         if (charIndex > 30) {
             charIndex -= 31;
         }
     }
     return treeCount;
+}
+
+// Solution for Part 2. You will be prompted for further input.
+// Also works as a solution to Part 1 if you check 1 route.
+const checkRoutes = () => {
+    const numberOfRoutes = prompt("How many routes do you want to check?");
+    if (numberOfRoutes === '') {
+        return "Sorry, you need to enter a valid number of routes."
+    }
+    const numberOfRoutesInt = parseInt(numberOfRoutes);
+
+    let treeCountArray = [];
+
+    for (route = 1; route < (numberOfRoutesInt + 1); route++) {
+        let right = prompt(`For Route ${route}, how many steps to the right?`);
+        if (right === '') {
+            return "Sorry, you need to enter a valid number of steps."
+        }
+        let rightInt = parseInt(right);
+
+        let down = prompt(`For Route ${route}, how many steps down?`);
+        if (down === '') {
+            return "Sorry, you need to enter a valid number of steps."
+        }
+        let downInt = parseInt(down);
+
+        let treeCount = countTrees(rightInt, downInt);
+        treeCountArray.push(treeCount);
+
+        right = '';
+        down = '';
+    }
+
+    return treeCountArray.reduce(multiplyTrees);
+
+}
+
+const multiplyTrees = (a, b) => {
+    return a * b;
 }
