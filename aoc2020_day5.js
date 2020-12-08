17c268687a5007da0cd343badb5c5f6b282b7445
phendev <stephen.bewers@gmail.com>
2020-12-08 18:42:14 +0000

    Solution to Day 5, Part 1

---
+++ aoc2020_day5.js
@@ -0,0 +1,56 @@
+const inputFile = "./aoc2020_day5_input.txt";
+let array = [];
+
+fetch(inputFile)
+  .then((response) => response.text())
+  .then((data) => {
+    array = data.split("\n");
+  });
+
+// Split a boarding pass ID into a row array and a column array and return both
+const getRowAndColumnArrays = (boardingPass) => {
+  let charArray = [...boardingPass];
+  let rowArray = [];
+  let columnArray = [];
+  if (charArray.length === 10) {
+    rowArray = charArray.slice(0, 7);
+    columnArray = charArray.slice(7, 10);
+  }
+  return [rowArray, columnArray];
+};
+
+// Gets the row or column number from a row or column array
+const getRowOrColumnNumber = (array, min, max) => {
+  for (item of array) {
+    if (item === "F" || item === "L") {
+      max = max - Math.ceil((max - min) / 2);
+    } else if (item === "B" || item === "R") {
+      min = min + Math.ceil((max - min) / 2);
+    } else {
+      throw "Invalid boarding pass!";
+    }
+  }
+  if (min === max) {
+    return min;
+  } else {
+    throw "Invalid boarding pass!";
+  }
+};
+
+// Gets an array of seat IDs from the array of boarding passes
+const getSeatIdArray = (array) => {
+  let seatIdArray = [];
+  for (boardingPass of array) {
+    const rowAndColumnArray = getRowAndColumnArrays(boardingPass.trim());
+    let rowNumber = getRowOrColumnNumber(rowAndColumnArray[0], 0, 127);
+    let columnNumber = getRowOrColumnNumber(rowAndColumnArray[1], 0, 7);
+    let seatId = rowNumber * 8 + columnNumber;
+    seatIdArray.push(seatId);
+  }
+  return seatIdArray;
+};
+
+const getMaxSeatId = (array) => {
+  const seatIdArray = getSeatIdArray(array);
+  return Math.max.apply(Math, seatIdArray);
+};
